e54ca0cf455274d50c52a9aa6e112976bb83edfb
Pavel <foujeupavel@gmail.com>
2019-09-07 23:10:18 +0100

    Adding project structure and setting up the burger and buttons to add or remove ingredients

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,47 @@
+import React  , {Component} from 'react'
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+
+class BurgerBuilder extends Component{
+
+    /*constructor(props){
+        super(props)
+        this.state = {...}
+    }*/
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon:0,
+            cheese:0,
+            meat:0
+        }
+    };
+
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCounted = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+    };
+
+    removeIngredientHandler = (type) => {
+
+    };
+
+
+    render() {
+        return (
+            <Aux>
+                <Burger ingredients = {this.state.ingredients}/>
+                <BuildControls/>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
